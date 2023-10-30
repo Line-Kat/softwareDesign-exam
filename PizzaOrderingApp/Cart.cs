88d3a2a7b3c230c8267875240eb890e014295a57
Emilie <emilie.haldorsen@hotmail.no>
2023-10-30 14:42:04 +0100

    added new classes for ordering (cart, order and queue). + Changed Program class

---
+++ PizzaOrderingApp/Cart.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PizzaOrderingApp {
+	public class Cart {
+
+		//pizzaName, count, Price
+		string pizza = "x,y,z";
+	    int timePerPizza = 10;
+		
+		
+		// methods: totalPrice, totalProductionTime
+	}
+}
--- PizzaOrderingApp/Program.cs
+++ PizzaOrderingApp/Program.cs
@@ -4,6 +4,15 @@
 			Console.WriteLine("Welcome to Pizza Factory");
 			Customer customer = new();
 			customer.AddCustomer();
+
+
+			// See menu 
+
+
+			// add to cart
+			Console.WriteLine("What do you want to order?");
+			String[] pizzaType = Console.ReadLine();
+			Console.WriteLine("Countinue to order (Y/N)");
 		}
 	}
 }
