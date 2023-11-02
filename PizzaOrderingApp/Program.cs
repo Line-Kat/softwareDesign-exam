0029704295221d667953e9b33f2f25c922f5def1
L_Kat <l_katralen@hotmail.com>
2023-11-02 18:02:30 +0100

    removed a few lines from the Program file

--- PizzaOrderingApp/Program.cs
+++ PizzaOrderingApp/Program.cs
@@ -4,12 +4,10 @@ namespace PizzaOrderingApp {
 	internal class Program {
 		static void Main(string[] args) {
 			Console.WriteLine("Welcome to Pizza Factory");
-			HandleCustomer handleCustomer = new();
-			handleCustomer.AddCustomer();
 
 			Customer customer = new();
 			Login login = new Login();
-			customer = login.userLogin(); //customer skal holde på den innloggede brukere
+			customer = login.userLogin(); //customer holder på den innloggede brukeren
 
 			Console.WriteLine($"Welcome {customer.CustomerName}");
 		}
