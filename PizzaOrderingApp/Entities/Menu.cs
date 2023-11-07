ee23b8fb0305fed0d5e45e2deec77bc1f64ab29f
danaithb <daha025@student.kristiania.no>
2023-11-07 14:30:15 +0100

    Added abstract Menu class

---
+++ PizzaOrderingApp/Entities/Menu.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PizzaOrderingApp.Entities
+{
+	public abstract class Menu
+	{
+
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public decimal Price { get; set; }
+		public virtual string Description { get; set; }
+
+		//Abstrakt metode så alle menyene bruker for å printe ut
+		public abstract void PrintMenu();
+
+	}
+}
