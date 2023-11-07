3b105f4562b7648646e87cb4050f51c0d0524108
danaithb <daha025@student.kristiania.no>
2023-11-07 14:49:50 +0100

    Made the printMenu() method for pizza

--- PizzaOrderingApp/Entities/Pizza.cs
+++ PizzaOrderingApp/Entities/Pizza.cs
@@ -5,13 +5,32 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace PizzaOrderingApp.Entities {
-	public class Pizza {
-		public int PizzaId { get; set; }
-		public string? PizzaName { get; set; }
-		public int Price { get; set; }
-		public string? Description { get; set; }
+	public class Pizza : Menu {
+		
 
 		//relasjon til koblingstabellen
 		public ICollection<Pizza_Order>? Pizza_Order { get; set; }
+
+
+		public override void PrintMenu()
+		{
+			Console.WriteLine("Here is the pizza menu:");
+
+			//fetche listen med pizza fra db
+			using (var db = new PizzaOrderingDbContext())
+			{
+				var pizzas = db.Pizza.ToList();
+				foreach (var pizza in pizzas)
+				{
+					Console.WriteLine($"Nr. {pizza.Id}");
+					Console.WriteLine($"Name: {pizza.Name}");
+					Console.WriteLine($"Price: {pizza.Price}kr"); // Formats the price as currency
+					Console.WriteLine($"Description: {pizza.Description}");
+
+				}
+			}
+				
+				
+		}
 	}
 }
