6bc073f0dba62d0e845d148f8b1f34ad18729637
danaithb <daha025@student.kristiania.no>
2023-11-09 14:24:37 +0100

    Test

--- PizzaOrderingApp/HandleOrder.cs
+++ PizzaOrderingApp/HandleOrder.cs
@@ -1,20 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using PizzaOrderingApp.Entities;
 
 namespace PizzaOrderingApp {
 	public class HandleOrder {
 
 		public void printOrder(Customer customer, DateTime dateTime) {
+			//kundenavn
 			//henter bestillingen fra cart klassen (mat/drikke, totalsum
+			//tidspunkt fra PizzaQueue
 			string name = customer.CustomerName;
 
 			Console.WriteLine($"Here is your receipt\n" +
 				$"Name: {customer.CustomerName}\n" +
 				$"Your order is ready for pick up at {dateTime}");
+
 		}
 
 		public Order addOrder(Customer customer) {
