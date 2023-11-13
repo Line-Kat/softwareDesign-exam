9fcfc335804083e75bd6558d2d8511cb46c8b079
danaithb <daha025@student.kristiania.no>
2023-11-13 12:44:43 +0100

    Added comments

--- PizzaOrderingApp/Program.cs
+++ PizzaOrderingApp/Program.cs
@@ -1,4 +1,5 @@
 using PizzaOrderingApp.Entities;
+using PizzaOrderingApp.MenuHandler;
 
 namespace PizzaOrderingApp
 {
@@ -7,16 +8,15 @@ namespace PizzaOrderingApp
 		static void Main(string[] args)
 		{
 
-			/*
-			 * Metode som legger til pizza i pizza tabellen i db (om de ikke finnes fra før av)
+			// Metode som legger til pizza i pizza tabellen i db (om de ikke finnes fra før av)
 				AddMenuItems addMenuItems = new AddMenuItems();
 				addMenuItems.AddItems();
 			
-			* Metode for å printe ut pizzamenyen
+			// Metode for å printe ut pizzamenyen
 				PizzaMenu menu = new PizzaMenu();
 				menu.PrintMenu();
 			 
-			 */
+			 /*
 
 			Console.WriteLine("Welcome to Pizza Factory");
 			HandleCustomer handleCustomer = new();
@@ -58,7 +58,7 @@ namespace PizzaOrderingApp
 					Console.WriteLine("You are now deleted from out database. You are welcome to come back another time");
 					keepRunning = false;
 				}
-			}
+			}*/
 		}
 	}
 }
