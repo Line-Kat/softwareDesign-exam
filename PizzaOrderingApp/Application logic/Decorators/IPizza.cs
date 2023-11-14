c544a65bba26422901155d170c21b7407e2d05b3
danaithb <daha025@student.kristiania.no>
2023-11-14 19:58:45 +0100

    Adding IPizza interface class

---
+++ PizzaOrderingApp/Application logic/Decorators/IPizza.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PizzaOrderingApp.Application_logic.Decorators
+{
+	public interface IPizza
+
+	{
+		string Description { get; }
+		int Price { get; }
+	}
+}
