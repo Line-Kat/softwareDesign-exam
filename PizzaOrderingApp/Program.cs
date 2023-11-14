1595cd60d777e0dc506b70c8bcc1391095552a0d
danaithb <daha025@student.kristiania.no>
2023-11-14 13:01:16 +0100

    Merge with main

--- PizzaOrderingApp/Program.cs
+++ PizzaOrderingApp/Program.cs
@@ -9,59 +9,58 @@ namespace PizzaOrderingApp
 		static void Main(string[] args)
 		{
 
-			// Metode som legger til pizza i pizza tabellen i db (om de ikke finnes fra før av)
+			/*
+			 * Metode som legger til pizza i pizza tabellen i db (om de ikke finnes fra før av)
 				AddMenuItems addMenuItems = new AddMenuItems();
 				addMenuItems.AddItems();
+			
+			* Metode for å printe ut pizzamenyen
+				PizzaMenu menu = new PizzaMenu();
+				menu.PrintMenu();
+			 
+			 */
 
-			// Displaye menyene
-			PizzaMenu pizzaMenu = new PizzaMenu();	
-			DisplayMenus displayMenus = new DisplayMenus(pizzaMenu); 
-			displayMenus.PrintMenu();
-
-
-			/*
-
-		   Console.WriteLine("Welcome to Pizza Factory");
-		   HandleCustomer handleCustomer = new();
-		   Customer customer = new();
+			Console.WriteLine("Welcome to Pizza Factory");
+			HandleCustomer handleCustomer = new();
+			Customer customer = new();
 
 
-		   Login login = new Login();
-		   customer = login.userLogin(); //customer holder på den innloggede brukeren
+			Login login = new Login();
+			customer = login.userLogin(); //customer holder på den innloggede brukeren
 
-		   Console.WriteLine($"Welcome {customer.CustomerName}");
+			Console.WriteLine($"Welcome {customer.CustomerName}");
 
 
-		   bool keepRunning = true;
-		   while (keepRunning)
-		   {
-			   Console.WriteLine("Choose an option:\n1 Order pizza\n2 Log out\n3 Delete your user");
-			   string userInput = Console.ReadLine();
-			   if (userInput.Equals("1"))
-			   {
-				   HandleOrder order = new();
-				   PizzaQueue queue = new PizzaQueue();
-				   DateTime dateTime = queue.CheckQueue();
-				   order.printOrder(customer, dateTime);
-				   order.addOrder(customer);
-				   keepRunning = false;
+			bool keepRunning = true;
+			while (keepRunning)
+			{
+				Console.WriteLine("Choose an option:\n1 Order pizza\n2 Log out\n3 Delete your user");
+				string userInput = Console.ReadLine();
+				if (userInput.Equals("1"))
+				{
+					HandleOrder order = new();
+					PizzaQueue queue = new PizzaQueue();
+					DateTime dateTime = queue.CheckQueue();
+					order.printOrder(customer, dateTime);
+					order.addOrder(customer);
+					keepRunning = false;
 
-			   }
+				}
 
-			   if (userInput.Equals("2"))
-			   {
-				   Console.WriteLine("Thank you for visiting us. Welcome back!");
-				   keepRunning = false;
+				if (userInput.Equals("2"))
+				{
+					Console.WriteLine("Thank you for visiting us. Welcome back!");
+					keepRunning = false;
 
-			   }
+				}
 
-			   if (userInput.Equals("3"))
-			   {
-				   handleCustomer.deleteCustomer(customer.CustomerId);
-				   Console.WriteLine("You are now deleted from out database. You are welcome to come back another time");
-				   keepRunning = false;
-			   }
-		   }*/
+				if (userInput.Equals("3"))
+				{
+					handleCustomer.deleteCustomer(customer.CustomerId);
+					Console.WriteLine("You are now deleted from out database. You are welcome to come back another time");
+					keepRunning = false;
+				}
+			}
 		}
 	}
 }
