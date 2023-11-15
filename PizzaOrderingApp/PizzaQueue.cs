11292f6e83e8d3581ea99978ca1a1900f8000c23
Emilie <emilie.haldorsen@hotmail.no>
2023-11-15 17:31:19 +0100

    .

--- PizzaOrderingApp/PizzaQueue.cs
+++ PizzaOrderingApp/PizzaQueue.cs
@@ -2,28 +2,60 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace PizzaOrderingApp {
 	public class PizzaQueue {
-		
 
-		public DateTime CheckQueue() {
+		/*
+		public int totalPizzaNumber { get; private set; }
+		public DateTime queueEnd { get; private set; }
+
+
+		public PizzaQueue(int pizzaNumber, DateTime newPizzaQueue) {
+			totalPizzaNumber += pizzaNumber;
+			queueEnd = newPizzaQueue;
+		}
+
+		*/
+		public static int totalNumberOfPizza(int userPizzaCount){
+			total += userPizzaCount;
+			return total;
+		}
+		
+		private static int getpizzaNumber() {
+			Console.WriteLine("Number of pizza");
+			int pizzaNumber = int.Parse(Console.ReadLine());
+			if (pizzaNumber <= 1) {
+				return pizzaNumber;
+			} else {
+				Console.WriteLine("Write a number (1 or higher)");
+				return getpizzaNumber();
+			}
+		}
+
+		public DateTime CheckQueue(int userPizzaCount) {
+			int totalPizzaNumber = 0;
+			// update queueWait with queueEnd and add userPizzaCount to totalPizzaNumber
+			totalPizzaNumber += userPizzaCount;
+			DateTime queueEnd = new DateTime(2023, 11, 09, 13, 20, 00);
+
 			DateTime timeNow = DateTime.Now;
-			DateTime queueEnd = new DateTime(2023, 11, 9, 07, 00, 00);
-			var pizzaCookingTime = new TimeSpan(0, 20, 0);
+			TimeSpan pizzaCookingTime = new TimeSpan(0, (userPizzaCount * 10), 0);
 
 			if (timeNow < queueEnd) {
 				DateTime queueWait = (queueEnd + pizzaCookingTime);
-				var queueWaitHours = (queueWait - timeNow);
+				Console.WriteLine(totalPizzaNumber);
 				return queueWait;
 			}
-			else if (timeNow > queueEnd) {
+			else if (timeNow >= queueEnd) {
 				DateTime queueWait = (timeNow + pizzaCookingTime);
 				return queueWait;
 			}
 			return timeNow;
+
 		}
 		// method to get number of minutes
 
