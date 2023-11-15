7609cc743bfa15e667f9e765499a8feff321abda
danaithb <daha025@student.kristiania.no>
2023-11-15 21:22:56 +0100

    Added some menu handler classes

--- PizzaOrderingApp/Program.cs
+++ PizzaOrderingApp/Program.cs
@@ -6,7 +6,8 @@ namespace PizzaOrderingApp
 {
 	internal class Program
 	{
-		static void Main(string[] args) {
+		static void Main(string[] args)
+		{
 
 			/*
 			 * Metode som legger til pizza i pizza tabellen i db (om de ikke finnes fra før av)
@@ -31,66 +32,77 @@ namespace PizzaOrderingApp
 
 
 			bool keepRunning = true;
-			while (keepRunning) {
+			while (keepRunning)
+			{
 				Console.WriteLine("Choose an option:\n1 Order pizza\n2 Log out\n3 Delete your user");
 				string userInput = Console.ReadLine();
-				if (userInput.Equals("1")) {
+				if (userInput.Equals("1"))
+				{
 					HandleOrder order = new();
 					PizzaQueue queue = new PizzaQueue();
 					bool NumInputIsEmpty = true;
 
-<<<<<<< HEAD
+
 					while (NumInputIsEmpty)
 					{
-=======
-					while (NumInputIsEmpty) {
->>>>>>> main
-						Console.WriteLine("Skriv antall pizzaer");
-						string countAsString = Console.ReadLine();
-						int userPizzaCount;
 
-<<<<<<< HEAD
-						if (!int.TryParse(countAsString, out userPizzaCount))
+						while (NumInputIsEmpty)
 						{
-							Console.WriteLine("Count needs to be higher than 1");
+
+							Console.WriteLine("Skriv antall pizzaer");
+							string countAsString = Console.ReadLine();
+							int userPizzaCount;
+
+							if (!int.TryParse(countAsString, out userPizzaCount))
+							{
+								Console.WriteLine("Count needs to be higher than 1");
+							}
+							else if (userPizzaCount <= 0)
+							{
+								Console.WriteLine("Count needs to be higher than 1");
+							}
+							else
+							{
+
+								if (!int.TryParse(countAsString, out userPizzaCount))
+								{
+									Console.WriteLine("Count needs to be higher than 1");
+								}
+								else if (userPizzaCount <= 0)
+								{
+									Console.WriteLine("Count needs to be higher than 1");
+								}
+								else
+								{
+
+									DateTime dateTime = queue.CheckQueue(userPizzaCount);
+									order.printOrder(customer, dateTime);
+									order.addOrder(customer);
+									NumInputIsEmpty = false;
+								}
+							}
+							keepRunning = false;
+
 						}
-						else if (userPizzaCount <= 0)
+
+						if (userInput.Equals("2"))
 						{
-							Console.WriteLine("Count needs to be higher than 1");
+							Console.WriteLine("Thank you for visiting us. Welcome back!");
+							keepRunning = false;
+
 						}
-						else
+
+						if (userInput.Equals("3"))
 						{
-=======
-						if (!int.TryParse(countAsString, out userPizzaCount)) {
-							Console.WriteLine("Count needs to be higher than 1");
-						} else if (userPizzaCount <= 0) {
-							Console.WriteLine("Count needs to be higher than 1");
-						} else {
->>>>>>> main
-							DateTime dateTime = queue.CheckQueue(userPizzaCount);
-							order.printOrder(customer, dateTime);
-							order.addOrder(customer);
-							NumInputIsEmpty = false;
+							handleCustomer.deleteCustomer(customer.CustomerId);
+							Console.WriteLine("You are now deleted from out database. You are welcome to come back another time");
+							keepRunning = false;
 						}
+
 					}
-					keepRunning = false;
 
 				}
-
-				if (userInput.Equals("2")) {
-					Console.WriteLine("Thank you for visiting us. Welcome back!");
-					keepRunning = false;
-
-				}
-
-				if (userInput.Equals("3")) {
-					handleCustomer.deleteCustomer(customer.CustomerId);
-					Console.WriteLine("You are now deleted from out database. You are welcome to come back another time");
-					keepRunning = false;
-				}
-
 			}
-
 		}
 	}
 }
