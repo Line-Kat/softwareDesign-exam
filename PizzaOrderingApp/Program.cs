632f2bb7f4e3c7b62ddfb65e50ea35d9a24452c4
L_Kat <l_katralen@hotmail.com>
2023-11-16 12:06:35 +0100

    fixed loop problem in Program

--- PizzaOrderingApp/Program.cs
+++ PizzaOrderingApp/Program.cs
@@ -34,76 +34,45 @@ namespace PizzaOrderingApp
 
 
 			bool keepRunning = true;
-			while (keepRunning)
-			{
+			while (keepRunning) {
 				Console.WriteLine("Choose an option:\n1 Order pizza\n2 Log out\n3 Delete your user");
 				string userInput = Console.ReadLine();
-				if (userInput.Equals("1"))
-				{
+				if (userInput.Equals("1")) {
 					HandleOrder order = new();
 					PizzaQueue queue = new PizzaQueue();
+
 					bool NumInputIsEmpty = true;
+					while (NumInputIsEmpty) {
 
+						Console.WriteLine("Skriv antall pizzaer");
+						string countAsString = Console.ReadLine();
+						int userPizzaCount;
 
-					while (NumInputIsEmpty)
-					{
-
-						while (NumInputIsEmpty)
-						{
-
-							Console.WriteLine("Skriv antall pizzaer");
-							string countAsString = Console.ReadLine();
-							int userPizzaCount;
-
-							if (!int.TryParse(countAsString, out userPizzaCount))
-							{
-								Console.WriteLine("Count needs to be higher than 1");
-							}
-							else if (userPizzaCount <= 0)
-							{
-								Console.WriteLine("Count needs to be higher than 1");
-							}
-							else
-							{
-
-								if (!int.TryParse(countAsString, out userPizzaCount))
-								{
-									Console.WriteLine("Count needs to be higher than 1");
-								}
-								else if (userPizzaCount <= 0)
-								{
-									Console.WriteLine("Count needs to be higher than 1");
-								}
-								else
-								{
-
-									DateTime dateTime = queue.CheckQueue(userPizzaCount);
-									order.printOrder(customer, dateTime);
-									order.addOrder(customer);
-									NumInputIsEmpty = false;
-								}
-							}
-							keepRunning = false;
-
+						if (!int.TryParse(countAsString, out userPizzaCount)) {
+							Console.WriteLine("Count needs to be higher than 1");
+						} else if (userPizzaCount <= 0) {
+							Console.WriteLine("Count needs to be higher than 1");
+						} else {
+							DateTime dateTime = queue.CheckQueue(userPizzaCount);
+							order.printOrder(customer, dateTime);
+							order.addOrder(customer);
+							NumInputIsEmpty = false;
 						}
 
-						if (userInput.Equals("2"))
-						{
-							Console.WriteLine("Thank you for visiting us. Welcome back!");
-							keepRunning = false;
-
-						}
-
-						if (userInput.Equals("3"))
-						{
-							handleCustomer.deleteCustomer(customer.CustomerId);
-							Console.WriteLine("You are now deleted from out database. You are welcome to come back another time");
-							keepRunning = false;
-						}
-
+						keepRunning = false;
 					}
-
 				}
+
+				if (userInput.Equals("2")) {
+					Console.WriteLine("Thank you for visiting us. Welcome back!");
+					keepRunning = false;
+				}
+
+				if (userInput.Equals("3")) {
+					handleCustomer.deleteCustomer(customer.CustomerId);
+					Console.WriteLine("You are now deleted from out database. You are welcome to come back another time");
+					keepRunning = false;
+				}	
 			}
 		}
 	}
