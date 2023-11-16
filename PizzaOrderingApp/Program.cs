95a2612cb4b95b2da69ff5306f53fc0af8d64704
L_Kat <l_katralen@hotmail.com>
2023-11-16 13:03:45 +0100

    fixed Program

--- PizzaOrderingApp/Program.cs
+++ PizzaOrderingApp/Program.cs
@@ -6,8 +6,7 @@ namespace PizzaOrderingApp
 {
 	internal class Program
 	{
-		static void Main(string[] args)
-		{
+		static void Main(string[] args) {
 
 			/*
 
@@ -34,14 +33,15 @@ namespace PizzaOrderingApp
 			bool keepRunning = true;
 			while (keepRunning) {
 				Console.WriteLine("Choose an option:\n1 Order pizza\n2 Log out\n3 Delete your user");
+
 				string userInput = Console.ReadLine();
 				if (userInput.Equals("1")) {
 					HandleOrder order = new();
-					PizzaQueue queue = new PizzaQueue();
+					PizzaQueue queue = new();
 
 					bool NumInputIsEmpty = true;
 					while (NumInputIsEmpty) {
-            
+
 						Console.WriteLine("Skriv antall pizzaer");
 						string countAsString = Console.ReadLine();
 						int userPizzaCount;
@@ -55,31 +55,23 @@ namespace PizzaOrderingApp
 							order.PrintOrder(customer, dateTime);
 							order.AddOrder(customer);
 							NumInputIsEmpty = false;
-							}
-							
-            keepRunning = false;
-
 						}
 
-						if (userInput.Equals("2"))
-						{
-							Console.WriteLine("Thank you for visiting us. Welcome back!");
-							keepRunning = false;
+						keepRunning = false;
+					}
+				}
 
-						}
-
-						if (userInput.Equals("3"))
-						{
-							handleCustomer.deleteCustomer(customer.CustomerId);
-							Console.WriteLine("You are now deleted from out database. You are welcome to come back another time");
-							keepRunning = false;
-
+				if (userInput.Equals("2")) {
+					Console.WriteLine("Thank you for visiting us. Welcome back!");
+					keepRunning = false;
+				}
 
 				if (userInput.Equals("3")) {
 					handleCustomer.DeleteCustomer(customer.CustomerId);
 					Console.WriteLine("You are now deleted from out database. You are welcome to come back another time");
 					keepRunning = false;
-				}	
+				}
+				
 			}
 		}
 	}
