ddf33fb299aa4faa7f198ee9720f8561a38e2519
danaithb <daha025@student.kristiania.no>
2023-11-26 16:03:03 +0100

    Testing test

--- PizzaOrderingApp.UnitTesting/Tests.cs
+++ PizzaOrderingApp.UnitTesting/Tests.cs
@@ -1,30 +1,22 @@
-﻿using System;
+﻿using Moq;
+using NUnit.Framework;
+using PizzaOrderingApp.MenuHandler;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using PizzaOrderingApp.Entities;
-using PizzaOrderingApp.Technical_services;
 
-namespace PizzaOrderingApp.UnitTesting {
-	public class Tests {
+namespace PizzaOrderingApp.Tests
+{
+	[Test]
+	public void AddCustomer_AddingCustomerToDb_ReturnsCustomer()
+	{
+		//Arrange
+		CrudOperationsCustomer crudOperationsCustomer = new();
+		string expectedName = "Dutleif";
 
+		//Act
+		Customer customer = crudOperationsCustomer.AddCustomer(new() { CustomerName = "Dutleif" });
+		string actualName = customer.CustomerName;
 
-
-		[Test]
-		public void AddCustomer_AddingCustomerToDb_ReturnsCustomer() {
-			//Arrange
-			CrudOperationsCustomer crudOperationsCustomer = new();
-			string expectedName = "Dutleif";
-
-			//Act
-			Customer customer = crudOperationsCustomer.AddCustomer(new() { CustomerName = "Dutleif" });
-			string actualName = customer.CustomerName;
-
-			//Assert
-			Assert.That(actualName, Is.EqualTo(expectedName));
-		}
-
-
+		//Assert
+		Assert.That(actualName, Is.EqualTo(expectedName));
 	}
 }
