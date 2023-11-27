481aa0bf8ff8c419c91901046ef0e756b5b4a67a
L_Kat <l_katralen@hotmail.com>
2023-11-27 22:00:45 +0100

    added number of pizzas to receipt

--- PizzaOrderingApp/Program.cs
+++ PizzaOrderingApp/Program.cs
@@ -8,20 +8,10 @@ namespace PizzaOrderingApp
 	{
 		static void Main(string[] args)
 		{
-
-			/*
-
-			
-			// Metode som legger til pizza i pizza tabellen i db (om de ikke finnes fra før av)
-			AddMenuItems addMenuItems = new AddMenuItems();
+	
+			//Metode som legger til pizza i pizza tabellen i db (om de ikke finnes fra før av)
+			AddMenuItems addMenuItems = new();
 			addMenuItems.AddItems();
-
-			// Displaye menyene
-
-			DisplayMenus displayMenus = new DisplayMenus();
-			displayMenus.PrintMenu();
-
-			*/
 
 			Console.WriteLine("Welcome to Pizza Factory");
 			HandleCustomer handleCustomer = new();
@@ -38,21 +28,24 @@ namespace PizzaOrderingApp
 				Console.WriteLine("\nChoose an option:\n1 Order pizza\n2 Log out\n3 Manage User");
 
 				string? userInput = Console.ReadLine();
-				if (userInput.Equals("1"))
-				{
+				if (userInput.Equals("1")) {
 					HandleOrder order = new();
 					PizzaQueue queue = new();
 
 					displayMenus.PrintMenu();
-					DateTime dateTime = queue.CheckQueue(handleOrder.GetNumberOfItems());
-					order.PrintOrder(customer, dateTime);
+
+					//her trenger vi å vite hvilke pizza brukeren har bestilt
+
+					int numberOfPizzas = handleOrder.GetNumberOfItems();
+
+					DateTime dateTime = queue.CheckQueue(numberOfPizzas);
+					order.PrintOrder(customer, dateTime, numberOfPizzas);
 					order.AddOrder(customer);
 
 					keepRunning = false;
 				}
 
-				if (userInput.Equals("2"))
-				{
+				if (userInput.Equals("2")) {
 					Console.WriteLine("Thank you for visiting us. Welcome back!");
 					keepRunning = false;
 				}
@@ -65,20 +58,17 @@ namespace PizzaOrderingApp
 						Console.WriteLine("\nManage your account:\n1 Edit your user \n2 Delete user");
 						string? userInput2 = Console.ReadLine();
 						if (userInput2.Equals("1")) {
-							handleCustomer.ConfirmAddCustomer(customer);
+							customer = handleCustomer.ConfirmCustomerInformation(customer);
 							keepRunning2 = false;
-						}
-
-						if (userInput2.Equals("2")) {
+						} else if (userInput2.Equals("2")) {
 							handleCustomer.DeleteCustomer(customer.CustomerId);
 							Console.WriteLine("You are now deleted from out database. You are welcome to come back another time");
 							keepRunning2 = false;
+						} else {
+							Console.WriteLine("\nChoose a number from the menu");
 						}
-
 					}
 				}
-				
-
 			}
 		}
 	}
