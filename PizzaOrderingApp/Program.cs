eeb642526d5ac751a16794bed8349b19d50e33bb
danaithb <daha025@student.kristiania.no>
2023-11-28 19:53:59 +0100

    Fixing merging conflicts

--- PizzaOrderingApp/Program.cs
+++ PizzaOrderingApp/Program.cs
@@ -1,7 +1,4 @@
-using PizzaOrderingApp.Application_logic.CartHandler;
-using PizzaOrderingApp.Application_logic.Decorators;
 using PizzaOrderingApp.Application_logic.MenuHandler;
-using PizzaOrderingApp.Application_logic.MenuHandler.Decorators;
 using PizzaOrderingApp.Entities;
 using PizzaOrderingApp.MenuHandler;
 
@@ -12,137 +9,77 @@ namespace PizzaOrderingApp
 		static void Main(string[] args)
 		{
 
-
-
-			// Metode som legger til pizza i pizza tabellen i db (om de ikke finnes fra før av)
-			AddMenuItems addMenuItems = new AddMenuItems();
+			//Metode som legger til pizza i pizza tabellen i db (om de ikke finnes fra før av)
+			AddMenuItems addMenuItems = new();
 			addMenuItems.AddItems();
-
-			ShoppingCart shoppingCart = new ShoppingCart();
-			DisplayMenus displayMenus = new DisplayMenus();
-			PizzaToppingSelectionHandler toppingHandler = new PizzaToppingSelectionHandler();
-			IPizza finalPizza = toppingHandler.GetFinalPizza();
-			CartMenu cartMenu = new CartMenu(shoppingCart, displayMenus, toppingHandler);
-
-			cartMenu.ShowMenu();
-
-
-
-
-
-
-
-
-			/*
-						// Displaye menyene
-						DisplayMenus displayMenus = new DisplayMenus();
-						ShoppingCart shoppingCart = new ShoppingCart();
-						PizzaToppingSelectionHandler toppingHandler = new PizzaToppingSelectionHandler();
-
-						// Anta at SelectPizza er en metode som lar brukeren velge en grunnpizza og returnerer en IPizza
-						IPizza basePizza = displayMenus.SelectPizza(); // Denne metoden må implementeres for å la brukeren velge en pizza
-						if (basePizza != null)
-						{
-							// La brukeren tilpasse pizzaen
-							IPizza selectedPizza = toppingHandler.HandleToppingSelection(basePizza);
-							// Legg den tilpassede pizzaen til i handlekurven
-							shoppingCart.AddPizzaToCart(selectedPizza);
-
-							// Vis CartMenu for videre interaksjoner
-							CartMenu cartMenu = new CartMenu(shoppingCart, displayMenus, toppingHandler);
-							cartMenu.ShowMenu();
-						}
-						else
-						{
-							Console.WriteLine("Ingen grunnpizza ble valgt. Avslutter programmet.");
-						}
-					}*/
-
-			/*		
 
 			Console.WriteLine("Welcome to Pizza Factory");
 			HandleCustomer handleCustomer = new();
-			Customer customer = new();
-
+			HandleOrder handleOrder = new();
+			DisplayMenus displayMenus = new();
 
 			Login login = new Login();
-			customer = login.userLogin(); //customer holder på den innloggede brukeren
+			Customer customer = login.UserLogin();
 
-		Console.WriteLine($"Welcome {customer.CustomerName}");
-
+			Console.WriteLine($"Welcome {customer.CustomerName}");
 
 			bool keepRunning = true;
-			while (keepRunning) {
-				Console.WriteLine("Choose an option:\n1 Order pizza\n2 Log out\n3 Delete your user");
-				string userInput = Console.ReadLine();
-				if (userInput.Equals("1")) {
+			while (keepRunning)
+			{
+				Console.WriteLine("\nChoose an option:\n1 Order pizza\n2 Log out\n3 Manage User");
+
+				string? userInput = Console.ReadLine();
+				if (userInput.Equals("1"))
+				{
 					HandleOrder order = new();
-					PizzaQueue queue = new PizzaQueue();
+					PizzaQueue queue = new();
 
-					bool NumInputIsEmpty = true;
-					while (NumInputIsEmpty) {
+					displayMenus.PrintMenu();
 
+					//her trenger vi å vite hvilke pizza brukeren har bestilt
 
-					while (NumInputIsEmpty)
+					int numberOfPizzas = handleOrder.GetNumberOfItems();
+
+					DateTime dateTime = queue.CheckQueue(numberOfPizzas);
+					order.PrintOrder(customer, dateTime, numberOfPizzas);
+					order.AddOrder(customer);
+
+					keepRunning = false;
+				}
+
+				if (userInput.Equals("2"))
+				{
+					Console.WriteLine("Thank you for visiting us. Welcome back!");
+					keepRunning = false;
+				}
+
+				if (userInput.Equals("3"))
+				{
+
+					bool keepRunning2 = true;
+					while (keepRunning2)
 					{
 
-						while (NumInputIsEmpty)
+						Console.WriteLine("\nManage your account:\n1 Edit your user \n2 Delete user");
+						string? userInput2 = Console.ReadLine();
+						if (userInput2.Equals("1"))
 						{
-
-							Console.WriteLine("Skriv antall pizzaer");
-							string countAsString = Console.ReadLine();
-							int userPizzaCount;
-
-							if (!int.TryParse(countAsString, out userPizzaCount))
-							{
-								Console.WriteLine("Count needs to be higher than 1");
-							}
-							else if (userPizzaCount <= 0)
-							{
-								Console.WriteLine("Count needs to be higher than 1");
-							}
-							else
-							{
-
-								if (!int.TryParse(countAsString, out userPizzaCount))
-								{
-									Console.WriteLine("Count needs to be higher than 1");
-								}
-								else if (userPizzaCount <= 0)
-								{
-									Console.WriteLine("Count needs to be higher than 1");
-								}
-								else
-								{
-
-									DateTime dateTime = queue.CheckQueue(userPizzaCount);
-									order.printOrder(customer, dateTime);
-									order.addOrder(customer);
-									NumInputIsEmpty = false;
-								}
-							}
-							keepRunning = false;
-
+							customer = handleCustomer.ConfirmCustomerInformation(customer);
+							keepRunning2 = false;
 						}
-
-						if (userInput.Equals("2"))
+						else if (userInput2.Equals("2"))
 						{
-							Console.WriteLine("Thank you for visiting us. Welcome back!");
-							keepRunning = false;
-
-						}
-
-						if (userInput.Equals("3"))
-						{
-							handleCustomer.deleteCustomer(customer.CustomerId);
+							handleCustomer.DeleteCustomer(customer.CustomerId);
 							Console.WriteLine("You are now deleted from out database. You are welcome to come back another time");
-							keepRunning = false;
+							keepRunning2 = false;
 						}
-
+						else
+						{
+							Console.WriteLine("\nChoose a number from the menu");
+						}
 					}
-
 				}
-			}*/
+			}
 		}
 	}
 }
