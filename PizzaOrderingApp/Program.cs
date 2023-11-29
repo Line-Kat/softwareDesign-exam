1742ee56c144e6c6c166e73f500b9fb7e23ae47e
danaithb <daha025@student.kristiania.no>
2023-11-30 00:27:03 +0100

    Organizing main method

--- PizzaOrderingApp/Program.cs
+++ PizzaOrderingApp/Program.cs
@@ -1,9 +1,4 @@
-using PizzaOrderingApp.Application_logic.CartHandler;
-using PizzaOrderingApp.Application_logic.Decorators;
-using PizzaOrderingApp.Application_logic.MenuHandler;
-using PizzaOrderingApp.Application_logic.MenuHandler.Decorators;
-using PizzaOrderingApp.Entities;
-using PizzaOrderingApp.MenuHandler;
+using PizzaOrderingApp.Application_runner;
 
 namespace PizzaOrderingApp
 {
@@ -12,59 +7,9 @@ namespace PizzaOrderingApp
 		static void Main(string[] args)
 		{
 
-			//Metode som legger til pizza i pizza tabellen i db (om de ikke finnes fra før av)
-			AddMenuItems addMenuItems = new();
-			addMenuItems.AddItems();
+			RunPizzaApp runPizzaApp = new();
+			runPizzaApp.Run();
 
-			Console.WriteLine("Welcome to Pizza Factory");
-			HandleCustomer handleCustomer = new();
-			HandleOrder handleOrder = new();
-			DisplayMenus displayMenus = new();
-			ShoppingCart shoppingCart = new ShoppingCart();
-			PizzaToppingSelectionHandler toppingHandler = new PizzaToppingSelectionHandler();
-			IPizza finalPizza = toppingHandler.GetFinalPizza();
-			CartMenu cartMenu = new CartMenu(shoppingCart, displayMenus, toppingHandler);
-
-			Login login = new Login();
-			Customer customer = login.UserLogin();
-
-			Console.WriteLine($"\nWelcome {customer.CustomerName}");
-
-			bool keepRunning = true;
-			while (keepRunning) {
-				Console.WriteLine("\nChoose an option:\n1. Order pizza\n2. Log out\n3. Manage your account");
-
-				string? userInput = Console.ReadLine();
-				if (userInput.Equals("1"))
-				{
-					HandleOrder order = new();
-					PizzaQueue queue = new();
-
-					cartMenu.ShowMenu();
-
-					//int numberOfPizzas= handleOrder.GetNumberOfItems();
-
-					int numberOfPizzas = handleOrder.GetNumberOfItems();
-
-					DateTime dateTime = queue.CheckQueue(numberOfPizzas);
-					order.PrintOrder(customer, dateTime, numberOfPizzas);
-					order.AddOrder(customer);
-
-					keepRunning = false;
-				}
-
-				if (userInput.Equals("2"))
-				{
-					Console.WriteLine("Thank you for visiting us. Welcome back!");
-					keepRunning = false;
-				}
-
-				if (userInput.Equals("3")) {
-					handleCustomer.DeleteCustomer(customer.CustomerId);
-					Console.WriteLine("You are now deleted from out database. You are welcome to come back another time");
-					keepRunning = false;
-				}
-			}
 		}
 	}
 }
