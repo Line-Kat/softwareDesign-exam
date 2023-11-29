3243465aff4e6ec7d983d05492717c39a7e96a09
L_Kat <l_katralen@hotmail.com>
2023-11-29 21:54:49 +0100

    formatting

--- PizzaOrderingApp/Application runner/RunPizzaApp.cs
+++ PizzaOrderingApp/Application runner/RunPizzaApp.cs
@@ -5,13 +5,10 @@ using PizzaOrderingApp.Application_logic.MenuHandler.Decorators;
 using PizzaOrderingApp.Entities;
 using PizzaOrderingApp.MenuHandler;
 
-namespace PizzaOrderingApp.Application_runner
-{
-	public class RunPizzaApp
-	{
+namespace PizzaOrderingApp.Application_runner {
+	public class RunPizzaApp {
 
-		public void Run()
-		{
+		public void Run() {
 
 			//Metode som legger til pizza i pizza tabellen i db (om de ikke finnes fra før av)
 			AddMenuItems addMenuItems = new();
@@ -24,7 +21,7 @@ namespace PizzaOrderingApp.Application_runner
 			ShoppingCart shoppingCart = new ShoppingCart();
 			PizzaToppingSelectionHandler toppingHandler = new PizzaToppingSelectionHandler();
 			IPizza finalPizza = toppingHandler.GetFinalPizza();
-			CartMenu cartMenu = new CartMenu(shoppingCart, displayMenus, toppingHandler);
+			CartMenu cartMenu = new(shoppingCart, displayMenus, toppingHandler);
 
 			Login login = new Login();
 			Customer customer = login.UserLogin();
@@ -32,19 +29,15 @@ namespace PizzaOrderingApp.Application_runner
 			Console.WriteLine($"\nWelcome {customer.CustomerName}");
 
 			bool keepRunning = true;
-			while (keepRunning)
-			{
+			while (keepRunning) {
 				Console.WriteLine("\nChoose an option:\n1. Order pizza\n2. Log out\n3. Manage your account");
 
 				string? userInput = Console.ReadLine();
-				if (userInput.Equals("1"))
-				{
+				if (userInput.Equals("1")) {
 					HandleOrder order = new();
 					PizzaQueue queue = new();
 
 					cartMenu.ShowMenu();
-
-					//int numberOfPizzas= handleOrder.GetNumberOfItems();
 
 					int totalNumberOfPizzas = shoppingCart.TotalNumberOfPizzas();
 					int totalToPay = shoppingCart.TotalToPay();
@@ -57,35 +50,27 @@ namespace PizzaOrderingApp.Application_runner
 					keepRunning = false;
 				}
 
-				if (userInput.Equals("2"))
-				{
+				if (userInput.Equals("2")) {
 					Console.WriteLine("Thank you for visiting us. Welcome back!");
 					keepRunning = false;
 				}
 
-				if (userInput.Equals("3"))
-				{
+				if (userInput.Equals("3")) {
 
 					bool keepRunning2 = true;
-					while (keepRunning2)
-					{
+					while (keepRunning2) {
 
 						Console.WriteLine("\nManage your account:\n1 Edit your user \n2 Delete user");
 						string? userInput2 = Console.ReadLine();
-						if (userInput2.Equals("1"))
-						{
+						if (userInput2.Equals("1")) {
 							customer = handleCustomer.ConfirmCustomerInformation(customer);
 							keepRunning2 = false;
-						}
-						else if (userInput2.Equals("2"))
-						{
+						} else if (userInput2.Equals("2")) {
 							handleCustomer.DeleteCustomer(customer.CustomerId);
 							Console.WriteLine("You are now deleted from out database. You are welcome to come back another time");
 							keepRunning2 = false;
 							keepRunning = false;
-						}
-						else
-						{
+						} else {
 							Console.WriteLine("\nChoose a number from the menu");
 						}
 					}
