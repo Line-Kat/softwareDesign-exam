fb6a751be9bf380a52b256b51403167c54aa9cc4
danaithb <daha025@student.kristiania.no>
2023-11-29 13:07:56 +0100

    -w Testing branch part, commenting out other parts

--- PizzaOrderingApp/Program.cs
+++ PizzaOrderingApp/Program.cs
@@ -1,4 +1,7 @@
+using PizzaOrderingApp.Application_logic.CartHandler;
+using PizzaOrderingApp.Application_logic.Decorators;
 using PizzaOrderingApp.Application_logic.MenuHandler;
+using PizzaOrderingApp.Application_logic.MenuHandler.Decorators;
 using PizzaOrderingApp.Entities;
 using PizzaOrderingApp.MenuHandler;
 
@@ -8,55 +11,67 @@ namespace PizzaOrderingApp
 	{
 		static void Main(string[] args)
 		{
-	
-			//Metode som legger til pizza i pizza tabellen i db (om de ikke finnes fra før av)
-			AddMenuItems addMenuItems = new();
+
+			// Metode som legger til pizza i pizza tabellen i db (om de ikke finnes fra før av)
+			AddMenuItems addMenuItems = new AddMenuItems();
 			addMenuItems.AddItems();
 
-			Console.WriteLine("Welcome to Pizza Factory");
-			HandleCustomer handleCustomer = new();
-			HandleOrder handleOrder = new();
-			DisplayMenus displayMenus = new();
+			ShoppingCart shoppingCart = new ShoppingCart();
+			DisplayMenus displayMenus = new DisplayMenus();
+			PizzaToppingSelectionHandler toppingHandler = new PizzaToppingSelectionHandler();
+			IPizza finalPizza = toppingHandler.GetFinalPizza();
+			CartMenu cartMenu = new CartMenu(shoppingCart, displayMenus, toppingHandler);
 
-			Login login = new Login();
-			Customer customer = login.UserLogin();
+			cartMenu.ShowMenu();
 
-			Console.WriteLine($"Welcome {customer.CustomerName}");
+			/*	//Metode som legger til pizza i pizza tabellen i db (om de ikke finnes fra før av)
+				AddMenuItems addMenuItems = new();
+				addMenuItems.AddItems();
 
-			bool keepRunning = true;
-			while (keepRunning) {
-				Console.WriteLine("Choose an option:\n1 Order pizza\n2 Log out\n3 Edit your user\n4 Delete your user");
+				Console.WriteLine("Welcome to Pizza Factory");
+				HandleCustomer handleCustomer = new();
+				HandleOrder handleOrder = new();
+				DisplayMenus displayMenus = new();
 
-				string userInput = Console.ReadLine();
-				if (userInput.Equals("1"))
-				{
-					HandleOrder order = new();
-					PizzaQueue queue = new();
+				Login login = new Login();
+				Customer customer = login.UserLogin();
 
-					displayMenus.PrintMenu();
+				Console.WriteLine($"Welcome {customer.CustomerName}");
 
-					//her trenger vi å vite hvilke pizza brukeren har bestilt
+				bool keepRunning = true;
+				while (keepRunning) {
+					Console.WriteLine("Choose an option:\n1 Order pizza\n2 Log out\n3 Edit your user\n4 Delete your user");
 
-					int numberOfPizzas = handleOrder.GetNumberOfItems();
+					string userInput = Console.ReadLine();
+					if (userInput.Equals("1"))
+					{
+						HandleOrder order = new();
+						PizzaQueue queue = new();
 
-					DateTime dateTime = queue.CheckQueue(numberOfPizzas);
-					order.PrintOrder(customer, dateTime, numberOfPizzas);
-					order.AddOrder(customer);
+						displayMenus.PrintMenu();
 
-					keepRunning = false;
-				}
+						//her trenger vi å vite hvilke pizza brukeren har bestilt
 
-				if (userInput.Equals("2")) {
-					Console.WriteLine("Thank you for visiting us. Welcome back!");
-					keepRunning = false;
-				}
+						int numberOfPizzas = handleOrder.GetNumberOfItems();
 
-				if (userInput.Equals("3")) {
-					handleCustomer.DeleteCustomer(customer.CustomerId);
-					Console.WriteLine("You are now deleted from out database. You are welcome to come back another time");
-					keepRunning = false;
-				}
-			}
+						DateTime dateTime = queue.CheckQueue(numberOfPizzas);
+						order.PrintOrder(customer, dateTime, numberOfPizzas);
+						order.AddOrder(customer);
+
+						keepRunning = false;
+					}
+
+					if (userInput.Equals("2")) {
+						Console.WriteLine("Thank you for visiting us. Welcome back!");
+						keepRunning = false;
+					}
+
+					if (userInput.Equals("3")) {
+						handleCustomer.DeleteCustomer(customer.CustomerId);
+						Console.WriteLine("You are now deleted from out database. You are welcome to come back another time");
+						keepRunning = false;
+					}
+				}*/
 		}
 	}
 }
