a3422ca2b1b1546636ac8f103833a37af2e2540d
danaithb <daha025@student.kristiania.no>
2023-11-29 20:48:54 +0100

    Fxing merge problems

--- PizzaOrderingApp/Program.cs
+++ PizzaOrderingApp/Program.cs
@@ -12,66 +12,84 @@ namespace PizzaOrderingApp
 		static void Main(string[] args)
 		{
 
-			// Metode som legger til pizza i pizza tabellen i db (om de ikke finnes fra før av)
-			AddMenuItems addMenuItems = new AddMenuItems();
+			//Metode som legger til pizza i pizza tabellen i db (om de ikke finnes fra før av)
+			AddMenuItems addMenuItems = new();
 			addMenuItems.AddItems();
 
+			Console.WriteLine("Welcome to Pizza Factory");
+			HandleCustomer handleCustomer = new();
+			HandleOrder handleOrder = new();
+			DisplayMenus displayMenus = new();
 			ShoppingCart shoppingCart = new ShoppingCart();
-			DisplayMenus displayMenus = new DisplayMenus();
 			PizzaToppingSelectionHandler toppingHandler = new PizzaToppingSelectionHandler();
 			IPizza finalPizza = toppingHandler.GetFinalPizza();
 			CartMenu cartMenu = new CartMenu(shoppingCart, displayMenus, toppingHandler);
 
-			cartMenu.ShowMenu();
+			Login login = new Login();
+			Customer customer = login.UserLogin();
 
-			/*	//Metode som legger til pizza i pizza tabellen i db (om de ikke finnes fra før av)
-				AddMenuItems addMenuItems = new();
-				addMenuItems.AddItems();
+			Console.WriteLine($"\nWelcome {customer.CustomerName}");
 
-				Console.WriteLine("Welcome to Pizza Factory");
-				HandleCustomer handleCustomer = new();
-				HandleOrder handleOrder = new();
-				DisplayMenus displayMenus = new();
+			bool keepRunning = true;
+			while (keepRunning)
+			{
+				Console.WriteLine("\nChoose an option:\n1. Order pizza\n2. Log out\n3. Manage your account");
 
-				Login login = new Login();
-				Customer customer = login.UserLogin();
+				string? userInput = Console.ReadLine();
+				if (userInput.Equals("1"))
+				{
+					HandleOrder order = new();
+					PizzaQueue queue = new();
 
-				Console.WriteLine($"Welcome {customer.CustomerName}");
+					cartMenu.ShowMenu();
 
-				bool keepRunning = true;
-				while (keepRunning) {
-					Console.WriteLine("Choose an option:\n1 Order pizza\n2 Log out\n3 Edit your user\n4 Delete your user");
+					//int numberOfPizzas= handleOrder.GetNumberOfItems();
 
-					string userInput = Console.ReadLine();
-					if (userInput.Equals("1"))
+					int totalNumberOfPizzas = shoppingCart.TotalNumberOfPizzas();
+					int totalToPay = shoppingCart.TotalToPay();
+					List<CartItem> cartItems = shoppingCart.GetCartItems();
+
+					DateTime dateTime = queue.CheckQueue(totalNumberOfPizzas);
+					order.PrintOrder(customer, dateTime, totalToPay, cartItems);
+					order.AddOrder(customer);
+
+					keepRunning = false;
+				}
+
+				if (userInput.Equals("2"))
+				{
+					Console.WriteLine("Thank you for visiting us. Welcome back!");
+					keepRunning = false;
+				}
+
+				if (userInput.Equals("3"))
+				{
+
+					bool keepRunning2 = true;
+					while (keepRunning2)
 					{
-						HandleOrder order = new();
-						PizzaQueue queue = new();
 
-						displayMenus.PrintMenu();
-
-						//her trenger vi å vite hvilke pizza brukeren har bestilt
-
-						int numberOfPizzas = handleOrder.GetNumberOfItems();
-
-						DateTime dateTime = queue.CheckQueue(numberOfPizzas);
-						order.PrintOrder(customer, dateTime, numberOfPizzas);
-						order.AddOrder(customer);
-
-						keepRunning = false;
+						Console.WriteLine("\nManage your account:\n1 Edit your user \n2 Delete user");
+						string? userInput2 = Console.ReadLine();
+						if (userInput2.Equals("1"))
+						{
+							customer = handleCustomer.ConfirmCustomerInformation(customer);
+							keepRunning2 = false;
+						}
+						else if (userInput2.Equals("2"))
+						{
+							handleCustomer.DeleteCustomer(customer.CustomerId);
+							Console.WriteLine("You are now deleted from out database. You are welcome to come back another time");
+							keepRunning2 = false;
+							keepRunning = false;
+						}
+						else
+						{
+							Console.WriteLine("\nChoose a number from the menu");
+						}
 					}
-
-					if (userInput.Equals("2")) {
-						Console.WriteLine("Thank you for visiting us. Welcome back!");
-						keepRunning = false;
-					}
-
-					if (userInput.Equals("3")) {
-						handleCustomer.DeleteCustomer(customer.CustomerId);
-						Console.WriteLine("You are now deleted from out database. You are welcome to come back another time");
-						keepRunning = false;
-					}
-				}*/
+				}
+			}
 		}
 	}
 }
